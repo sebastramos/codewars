95fa01455cfdf810bcde1cb5f4ae0ced95174f0e
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-03 18:58:39 +0300

    codewars challenge

---
+++ 8kuy/string-repeat.js
@@ -0,0 +1,11 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr(n, s) {
+	let result = ''
+	for(let i = 0; i < n; i++) {
+    result += s
+  }
+  return result
+}
+
+console.log(repeatStr(3, ' ola'))
