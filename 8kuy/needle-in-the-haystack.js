5af9fbceb9d2ab357784f9070cf1cfcdfb89ba83
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-10 11:22:27 +0300

    codewars challenge

---
+++ 8kuy/needle-in-the-haystack.js
@@ -0,0 +1,14 @@
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+	for(let i = 1; i < haystack.length; i++)
+  	if(haystack[i] === 'needle') {
+    	return `Found the needle at position ${i}`
+    }
+}
+
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
