f0e9e2f4cfe7c1ab3036fffeeb5348db0aa32fe0
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-10 11:24:01 +0300

    codewars challenge

--- 8kuy/needle-in-the-haystack.js
+++ 8kuy/needle-in-the-haystack.js
@@ -12,3 +12,12 @@ function findNeedle(haystack) {
 }
 
 console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
+
+
+/* Better solution
+
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+
+*/
