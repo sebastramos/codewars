3aeb5d7abe2afe22e6cd5046b1a4b4c2d545e6d2
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-11 14:26:11 +0300

    codewas challenge

---
+++ 8kuy/sum-arrays.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array of numbers and returns the sum of the numbers. 
+// The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {
+  "use strict";
+  return numbers.reduce((a,b) => a + b, 0)
+};
+
+console.log(sum([1, 5.2, 4, 0, -1]))
+// 9.2
+
+console.log(sum([]))
+// 0
+
+console.log(sum([-2.398]))
+//-2.398
