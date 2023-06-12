61667867c43acac1e08ff121ad7bbd9ecb5aa4cd
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-12 23:17:50 +0300

    codewars challenge

---
+++ 8kuy/calculate-average.js
@@ -0,0 +1,7 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+
+let findAverage = array => array.reduce((acc,curr) => a + b,0) / array.length || 0
