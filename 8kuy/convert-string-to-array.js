ac76e01be6d4148695bdb05cb0c62860beef4022
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-17 22:52:56 +0300

    codewars challenge

---
+++ 8kuy/convert-string-to-array.js
@@ -0,0 +1,8 @@
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+  return string.split(' ');
+}
+
+console.log(stringToArray("I love arrays they are my favorite"))
+//["I", "love", "arrays", "they", "are", "my", "favorite"]
