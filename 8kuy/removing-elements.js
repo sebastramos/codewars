27d02be9d501c9a8091c2ff78863896f1be806f0
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-24 18:14:18 +0300

    codewars challenge

---
+++ 8kuy/removing-elements.js
@@ -0,0 +1,8 @@
+// Take an array and remove every second element from the array.
+// Always keep the first element and start removing with the next element.
+
+function removeEveryOther(arr){
+  return arr.filter(function(elem, index) {
+    return index % 2 === 0;
+  });
+}
