200c84cc291db8eda1ecdb149d5418f88c941e9b
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-26 19:03:38 +0300

    codewars challenge

---
+++ 8kuy/all-star-code-challenge-18.js
@@ -0,0 +1,31 @@
+// Create a function that accepts a string and a single character, 
+// and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+function strCount(str, letter){  
+	let count = 0
+  for(let i = 0; i < str.length; i++) {
+  	if (str[i] === letter) {
+    	count++
+    }
+  }
+  return count
+}
+
+console.log(strCount("Hello", 'o'))
+// returns 1
+console.log(strCount("Hello", 'l'))
+// returns 2
+console.log(strCount("", 'z'))
+// returns 0
+
+
+// Another solution !
+function strCount(str, letter){  
+  return str.split('').filter(c => c == letter).length;
+}
+
+
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
