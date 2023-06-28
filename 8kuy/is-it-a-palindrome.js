3ca1d8cf6d1334e6cf7d1521edb2458cf3572caa
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-28 22:23:17 +0300

    codewars challenge

---
+++ 8kuy/is-it-a-palindrome.js
@@ -0,0 +1,27 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome. 
+// A palindrome is a word, number, phrase, 
+// or other sequence of symbols that reads the same backwards as forwards,
+// such as madam or racecar, 
+// the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+
+function isPalindrome(x) {
+  return x.toLowerCase() === x.split('').reverse().join('').toLowerCase()
+ }
+ 
+ 
+ console.log(isPalindrome("a"))
+ // true
+ console.log(isPalindrome("aba"))
+ // true
+ console.log(isPalindrome("Abba"))
+ // true
+ console.log(isPalindrome("hello"))
+ // false
+ console.log(isPalindrome("Bob"))
+ // true
+ console.log(isPalindrome("Madam"))
+ // true
+ console.log(isPalindrome("AbBa"))
+ // true
+ console.log(isPalindrome(""))
+ // true
