bf0ba297e955ef6fbdc880a33fda4175477290b9
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-04 13:59:58 +0300

    codewars challenge

---
+++ 8kuy/name-shuffler.js
@@ -0,0 +1,8 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+	return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler("john McClane"))
+// "McClane john"
