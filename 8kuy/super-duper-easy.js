9a1b662072c8ddca6e546e82fd47f02a5d07b9ff
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-08 21:52:54 +0300

    codewars challenge

---
+++ 8kuy/super-duper-easy.js
@@ -0,0 +1,33 @@
+// Make a function that returns the value multiplied by 50 and increased by 6.
+// If the value entered is a string it should return "Error".
+
+// Solution 1
+function problem(x){
+  return x === Number(x) ? x * 50 + 6 : 'Error'
+}
+
+// Solution 2
+function problem(x){
+  return typeof x === 'number' ? x * 50 + 6 : 'Error'
+}
+
+console.log(problem("hello"))
+// "Error"
+console.log(problem(1))
+// 56
+console.log(problem(5))
+// 256
+console.log(problem(0))
+// 6
+console.log(problem(1.2))
+// 66
+console.log(problem(3))
+// 156
+console.log(problem("RyanIsCool"))
+// "Error"
+console.log(problem(-3))
+// -144
+console.log(problem(""))
+// "Error"
+console.log(problem(0.03))
+// 7.5
