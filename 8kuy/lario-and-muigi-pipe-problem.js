31c20d766f6215a6f27e6fa00199e7ac42faa43b
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-08 22:03:37 +0300

    codewars challenge

---
+++ 8kuy/lario-and-muigi-pipe-problem.js
@@ -0,0 +1,21 @@
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+function pipeFix(n){
+	let min = Math.min(...n)
+  let max = Math.max(...n)
+  let result = []
+  
+  for (let i = min; i <= max; i++) {
+  	result.push(i)
+  }
+  return result
+}
+
+console.log(pipeFix([1,3,5,6,7,8]))
+// 1,2,3,4,5,6,7,8
