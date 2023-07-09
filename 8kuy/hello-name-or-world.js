f9e499a7a3cc72e5f4c3c8e6d33da48a21e9ec3f
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-09 17:54:23 +0300

    codewars challenge

---
+++ 8kuy/hello-name-or-world.js
@@ -0,0 +1,21 @@
+// Define a method hello that returns "Hello, Name!" to a given name, 
+// or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Solution 1
+function hello(name) {
+  return (name === '' || name === undefined) ? 'Hello, World!' : `Hello, ${name.charAt(0).toUpperCase() + name.toLowerCase().substr(1)}!`
+}
+
+// Solution 2
+const hello = s =>
+  `Hello, ${s ? (s[0].toUpperCase() + s.slice(1).toLowerCase()) : 'World'}!`
+
+console.log(hello("john"))
+// "Hello, John!"
+console.log(hello("aliCE"))
+// "Hello, Alice!"
+console.log(hello(''))
+// "Hello, World!"
+console.log(hello())
