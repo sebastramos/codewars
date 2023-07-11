4e5f33eb899c0f57a94a1213d6e90fded61d9b85
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-11 17:24:49 +0300

    codewars challenge

---
+++ 8kuy/function-syntax-debugging.js
@@ -0,0 +1,15 @@
+// A student was working on a function and made some syntax mistakes while coding. 
+// Help them find their mistakes and fix them.
+
+// Solution 1
+const main = (verb, noun) => verb + noun
+
+// Solution 2
+function main (verb, noun) {
+  return verb + noun
+}
+
+console.log(main('take ', 'item'))
+// 'take item'
+console.log(main('use ', 'sword'))
+// 'use sword'
