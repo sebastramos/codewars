baf4d354dfb003c11367d618fe08cac0cada147e
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-11 16:21:57 +0300

    codewars challenge

---
+++ 8kuy/bin-to-decimal.js
@@ -0,0 +1,12 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+	return parseInt(bin, 2)
+}
+
+console.log(binToDec("1"))
+// 1
+console.log(binToDec("0"))
+// 0
+console.log(binToDec("1001001"))
+// 73
