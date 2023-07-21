4a9a17f40e8696088e448272958b683b69e526ed
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-22 00:13:13 +0300

    codewars challenge

---
+++ 8kuy/pillars.js
@@ -0,0 +1,21 @@
+// There are pillars near the road.
+// The distance between the pillars is the same and the width of the pillars is the same.
+// Your function accepts three arguments:
+
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+function pillars(numPill, dist, width) {
+
+return numPill <= 1 ? 0 : ((numPill - 1) * dist * 100) + (width * numPill) - (2 * width)
+}
+
+console.log(pillars(1, 10, 10))
+// 0
+console.log(pillars(2, 20, 25))
+// 2000
+console.log(pillars(11, 15, 30))
+// 15270
