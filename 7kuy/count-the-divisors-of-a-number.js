7ea2587105f87f2786f053deb5b427a8ae4d14bb
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-28 22:50:59 +0300

    codewars challenge

--- 7kuy/count-the-divisors-of-a-number.js
+++ 7kuy/count-the-divisors-of-a-number.js
@@ -2,26 +2,17 @@
 
 // Random tests go up to n = 500000.
 
-// Solution 1 (Slow)
+// Solution 1
 function getDivisorsCnt(n){
-  let divisor = []
-    for(let i = 0; i <= n; i++) {
+  let divisor = 0
+    for(let i = 1; i <= n; i++) {
       if(n % i === 0) {
-        divisor.push(i)
+        divisor++
       }
     }
-    return divisor.length
+    return divisor
   }
-  
-  // Solution 2
-  function getDivisorsCnt(n){
-  // loop that create n element in array
-  let number = []
-  for(let i = 1; i <= n; i++) {
-     number.push(BigInt(i))
-  }
-  return number.filter(el => n % el === 0).length
-  }
+
   
   console.log(getDivisorsCnt(4))
   // --> 3 // we have 3 divisors - 1, 2 and 4
@@ -32,6 +23,7 @@ function getDivisorsCnt(n){
   console.log(getDivisorsCnt(30 ))
   //--> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
   console.log(getDivisorsCnt(500000))
+  // 42
   
   // Note you should only return a number, the count of divisors.
   // The numbers between parentheses are shown only for you to see which numbers are counted in each case.
