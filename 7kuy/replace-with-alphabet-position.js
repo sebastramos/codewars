01a3edf55b4d4970d9310b89a2283cb0b7a06c93
Sebastian Ramos <sebramos.dev@gmail.com>
2023-08-01 23:37:11 +0300

    codewars challenge

--- 7kuy/replace-with-alphabet-position.js
+++ 7kuy/replace-with-alphabet-position.js
@@ -39,5 +39,12 @@ function alphabetPosition(text) {
   return position.join(' ')
 }
 
+
+// Solution 2
+function alphabetPosition(text) {
+  return text.toLowerCase().split('').map(a => a.charCodeAt(0) - 96).filter(a => a > 0 && a < 27).join(' ');
+}
+
+
 console.log(alphabetPosition("The sunset sets at twelve o' clock."))
 // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
