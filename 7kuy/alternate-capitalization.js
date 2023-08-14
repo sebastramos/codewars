baac78e405892748c036a745490d363c0e36cbc0
Sebastian Ramos <sebramos.dev@gmail.com>
2023-08-14 20:57:59 +0300

    codewars challenge

--- 7kuy/alternate-capitalization.js
+++ 7kuy/alternate-capitalization.js
@@ -5,10 +5,29 @@
 
 // The input will be a lowercase string with no spaces.
 
+// Solution 1
 function capitalize(s){
   const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("")
   const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("")
   return [even, odd];
+}
+
+// Solution 2
+function capitalize(s) {
+  const arr = s.split('');
+  let a = '', b = '';
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2) {
+      a += arr[i];
+      b += arr[i].toUpperCase()
+    } else {
+      a += arr[i].toUpperCase()
+      b += arr[i]
+    }
+  }
+  
+  return [a, b];
 }
 
 console.log(capitalize("abcdef"))
