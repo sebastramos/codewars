8e7b7f16d5e8aef58732018601118033920d3e3a
Sebastian Ramos <sebramos.dev@gmail.com>
2023-08-14 18:52:13 +0300

    codewars challenge

---
+++ 7kuy/no-oddities-here.js
@@ -0,0 +1,13 @@
+// Write a small function that returns the values of an array that are not odd.
+// All values in the array will be integers.
+// Return the good values in the order they are given.
+
+function noOdds( values ){
+	return values.filter(n => n % 2 === 0)
+}
+
+console.log(noOdds( [0,1] ))
+// [0]
+console.log(noOdds( [0,1,2,3] ))
+// [0,2]
+
