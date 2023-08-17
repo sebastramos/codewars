e94066256347813086e120171f258886cfc5182b
Sebastian Ramos <sebramos.dev@gmail.com>
2023-08-17 20:40:24 +0300

    codewars challenge

---
+++ 8kuy/remove-the-time.js
@@ -0,0 +1,13 @@
+// You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+// You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+// Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+function shortenToDate(longDate) {
+	longDate = longDate.split(',')
+  return longDate[0]
+}
+
+console.log(shortenToDate("Friday May 2, 7pm"))
+// "Friday May 2"
