f0b0375d121c21c3515f3f468a8a3b961f78fe50
Sebastian Ramos <sebramos.dev@gmail.com>
2023-09-07 12:50:16 +0800

    codewars challenge

---
+++ 8kuy/get-ascii-value-of-character.js
@@ -0,0 +1,12 @@
+// Get ASCII value of a character.
+
+function getASCII(c){
+	return c.charCodeAt()
+}
+
+console.log(getASCII('A'))
+// 65
+console.log(getASCII(' '))
+// 32
+console.log(getASCII('!'))
+// 33
