94723eb1fd8d4fc68c54dbc424ed505627bd9898
Sebastian Ramos <sebramos.dev@gmail.com>
2023-09-13 20:29:55 +0800

    codewars challenge

---
+++ 7kuy/currying-functions-multiply-all-elements-in-an-array.js
@@ -0,0 +1,16 @@
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. 
+
+// This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+
+// function multi
+
+const multiplyAll = arr => 
+                       n => arr.map(x => n * x)
+                       
+console.log(multiplyAll([1, 2, 3])(2))
+// [2, 4, 6]
+
+// You must not mutate the original array.
