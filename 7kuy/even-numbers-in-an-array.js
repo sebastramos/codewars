14762ec40a62cae228dd3dc241697a78606af4ff
Sebastian Ramos <sebramos.dev@gmail.com>
2023-09-13 14:18:36 +0800

    codewars challenge

---
+++ 7kuy/even-numbers-in-an-array.js
@@ -0,0 +1,15 @@
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). 
+
+// The original array will be not empty and will contain at least "number" even numbers.
+
+function evenNumbers(array, number) {
+	array = array.filter(n => n % 2 === 0)
+  return array.slice(-number)
+}
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+// [4, 6, 8]
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
+// [-8, 26]
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
+// [6]
