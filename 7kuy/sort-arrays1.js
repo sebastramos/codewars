fdd21f9c69fd52c2f3fc06400346e3b96c36a4e5
Sebastian Ramos <sebramos.dev@gmail.com>
2023-09-13 14:05:39 +0800

    codewars challenge

---
+++ 7kuy/sort-arrays1.js
@@ -0,0 +1,9 @@
+// Just a simple sorting usage. 
+// Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+let sortme = function(names) {
+	return names.sort()
+}
+
+console.log(sortme(['one', 'two', 'three' ]))
+// ["one", "three", "two"])
