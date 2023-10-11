9d4bb7666db4892bdc3ad077080b923f927acd66
Sebastian Ramos <sebramos.dev@gmail.com>
2023-10-11 22:27:21 +0300

    codewars challenge

---
+++ 8kuy/price-of-mangoes.js
@@ -0,0 +1,32 @@
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. 
+// For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+// Solution 1
+function mango(quantity, price){
+	let count = 0
+	for(let i = 0; i <= quantity; i++) {
+  	if(i % 3 !== 0) {
+    	count++
+    }
+  }
+  return count * price
+}
+
+// Solution 2
+function mango(quantity, price){
+  let freeMangos = Math.floor(quantity / 3)
+return (quantity - freeMangos) * price
+}
+
+console.log(mango(2, 3))
+// 6
+//  2 mangoes for $3 per unit = $6; no mango for free
+console.log(mango(3, 3)) 
+// 6    
+//  2 mangoes for $3 per unit = $6; +1 mango for free
+console.log(mango(5, 3))
+// 12   
+// 4 mangoes for $3 per unit = $12; +1 mango for free
+console.log(mango(9, 5)) 
+// 30   
+// 6 mangoes for $5 per unit = $30; +3 mangoes for free
