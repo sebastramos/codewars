276a06e402e247be9f9b6a842621fc215a12dffd
Sebastian Ramos <sebramos.dev@gmail.com>
2023-10-14 18:22:03 +0300

    codewars challenge

---
+++ 7kuy/return-the-missing-element.js
@@ -0,0 +1,32 @@
+// Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+
+// Solution 1
+function getMissingElement(arr){
+  let check = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  arr = arr.sort()
+  
+  for(let i = 0; i < check.length; i++) {
+    if(arr[i] !== check[i]) {
+      return check[i]
+    }
+  }
+}
+
+// Solution 2
+function getMissingElement(arr){
+  for (i = 0; i < 10; i++) {
+  // i will increment to 0 => 9 
+  // if it doesn't find the indexOf(i) in arr it return -1
+    if (superImportantArray.indexOf(i) === -1) return i;
+  }
+}
+
+console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]))
+// 8
+console.log(getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1]))
+// 3
