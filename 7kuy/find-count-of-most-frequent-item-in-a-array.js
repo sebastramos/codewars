b199cafd2482364cf359ae5e73e377a264d0f9d8
Sebastian Ramos <sebramos.dev@gmail.com>
2023-10-16 14:27:52 +0300

    codewars challenge

--- 7kuy/find-count-of-most-frequent-item-in-a-array.js
+++ 7kuy/find-count-of-most-frequent-item-in-a-array.js
@@ -32,6 +32,17 @@ function mostFrequentItemCount(collection) {
 }
 
 // Solution 2
+function mostFrequentItemCount(collection) {
+  if (collection.length === 0) return 0
+  
+  var count = Object.create(null)
+  
+  collection.forEach( item => {
+    count[item] = (count[item] || 0) + 1;
+  })
+  
+  return Math.max(...Object.values(count))
+}
 
 console.log(mostFrequentItemCount([]))
 // 0
