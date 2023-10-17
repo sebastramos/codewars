e105528c5d284e331d298a42ef70be6d31dabf32
Sebastian Ramos <sebramos.dev@gmail.com>
2023-10-17 20:49:50 +0300

    codewars challenge

--- 8kuy/replace-all-vowel-to-exclamation-mark.js
+++ 8kuy/replace-all-vowel-to-exclamation-mark.js
@@ -1,6 +1,8 @@
 // Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
 // a, e, i, o, u
+
+// Solution 1
 function replace(s){
   let a = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
   let arr = s.split('')
@@ -10,6 +12,26 @@ function replace(s){
       }
   }
   return arr.join('')
+}
+
+// Solution 2
+function replace(s){
+	let vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+  s = s.split('')
+  
+  for(let i = 0; i < s.length; i++) {
+  	for(let j = 0; j < vowels.length; j++) {
+    	if(s[i] === vowels[j]) {
+      	s[i] = '!'
+      }
+    }
+  }
+  return s.join('')
+}
+
+// Solution 3
+function replace (s) {
+	return (s.split('').map(e => 'aeiou'.includes(e.toLowerCase()) ? '!' : e)).join('')
 }
 
 console.log(replace("Hi!"))
