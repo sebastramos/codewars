7b86164e39f30ac10173ec08058912bf4cad9f5a
Sebastian Ramos <sebramos.dev@gmail.com>
2023-11-06 10:17:03 +0200

    codewars challenge

---
+++ 8kuy/subclasses-adam-and-eve.js
@@ -0,0 +1,37 @@
+// According to the creation myths of the Abrahamic religions,
+// Adam and Eve were the first Humans to wander the Earth.
+
+// You have to do God's job.
+// The creation method must return an array of length 2 containing objects (representing Adam and Eve). 
+// The first object in the array should be an instance of the class Man.
+// The second should be an instance of the class Woman.
+// Both objects have to be subclasses of Human.
+// Your job is to implement the Human, Man and Woman classes.
+
+
+class God {
+  static create() {
+  	return [new Man('Adam'), new Women('Eve')]	
+  }
+}
+
+class Human {
+	constructor(name) {
+  	this._name = name
+  }
+}
+
+class Man extends Human {
+	constructor(name) {
+  	super(name)
+  }
+}
+
+class Women extends Human {
+	constructor(name) {
+  	super(name)
+  }
+}
+
+
+console.log(God.create())
