3ec291f0b44029edde349c566e7bea98a20ebdce
Sebastian Ramos <sebramos.dev@gmail.com>
2023-11-26 10:14:48 +0200

    codewars challenge

---
+++ 6kuy/title-case.js
@@ -0,0 +1,28 @@
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+// Arguments 
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string.
+// The JavaScript tests will pass undefined when this argument is unused.
+
+function titleCase(title, minorWords) {
+  const minorArr = minorWords ? minorWords.toLowerCase().split(' ') : []
+  return title.toLowerCase()
+    .split(' ')
+    .map((word, i) => {
+      if (!word) return word
+      if (minorArr.indexOf(word) !== -1 && i !== 0) return word
+      
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+}
+
+console.log(titleCase('a clash of KINGS', 'a an the of'))
+// 'A Clash of Kings'
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'))
+// 'The Wind in the Willows'
+console.log(titleCase('the quick brown fox'))
+// 'The Quick Brown Fox
