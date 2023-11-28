14aee2d80890ea6d387ac3a4fcc077f8b6656b09
Sebastian Ramos <sebramos.dev@gmail.com>
2023-11-28 14:22:18 +0200

    codewars challenge

--- 6kuy/backspaces-in-string.js
+++ 6kuy/backspaces-in-string.js
@@ -18,7 +18,7 @@ function cleanString(s) {
 
 // Solution 2
 function cleanString(s) {
-  let result = []
+  let result = [];
   [...s].map((char) => (char === "#" ? result.pop() : result.push(char)))
   return result.join("")
 }
