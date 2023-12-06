1575300c4fa29486a34f33632427154705dbc55d
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-06 11:58:39 +0200

    codewars challenge

--- 8kuy/collatz-conjecture-3n+1.js
+++ 8kuy/collatz-conjecture-3n+1.js
@@ -22,6 +22,15 @@ function hotpo(n) {
   return count
 }
 
+// Solution 2
+let hotpo = (n, acc = 0) => {
+  if (n <= 1) {
+    return acc;
+  } else {
+    return hotpo(n%2===0 ? n/2 : 3*n+1, acc+1);
+  }
+}
+
 console.log(hotpo(1))
 // 0
 // (1 is already 1)
