1928b9f0a6cea7b889673937a080884396bea55a
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-06 12:13:47 +0200

    codewars challenge

---
+++ 8kuy/method-of-string-object-concat-split-and-join.js
@@ -0,0 +1,17 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+function splitAndMerge(str, sep) {
+	return str.split(' ').map(word => word.split('').join(sep)).join(' ')
+}
+
+console.log(splitAndMerge("My name is John", " "))
+// "M y n a m e i s J o h n"
+console.log(splitAndMerge("My name is John", "-"))
+// "M-y n-a-m-e i-s J-o-h-n"
+console.log(splitAndMerge("Hello World!", "."))
+// "H.e.l.l.o W.o.r.l.d.!"
+console.log(splitAndMerge("Hello World!", ","))
+// "H,e,l,l,o W,o,r,l,d,!"
