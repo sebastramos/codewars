9fbed5c1d75d8ea7b206af8f6c942cea6eb478f2
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-06 13:06:19 +0200

    codewars challenge

---
+++ 8kuy/loop-statement-for-in-for-of.js
@@ -0,0 +1,31 @@
+// Task
+// The function giveMeFive accepts 1 parameter, obj, which is an object.
+
+// Create an array (which you will eventually return).
+// Then, traverse obj, checking each key and value.
+
+// If the length of the key is equal to 5, then push the key to your array. 
+// Separately, if the length of the value is equal to 5, then push the value to your array.
+
+// At the end, return your array.
+// You should use for..in in your code, otherwise your solution may not pass this kata.
+
+function giveMeFive(obj) {
+	let arr = []
+  
+  for(let n in obj) {
+  	if(n.length === 5) {
+    	arr.push(n)
+    } if(obj[n].length === 5) {
+    	arr.push(obj[n])
+    }
+  }
+  return arr
+}
+
+console.log(giveMeFive({Our:"earth",is:"a",beautyful:"world"}))
+// ["earth","world"]
+console.log(giveMeFive({Ihave:"enough", money:"to",buy:"a",car:"model"}))
+// ["Ihave","money","model"]
+console.log(giveMeFive({Pears:"than",apple:"sweet"}))
+// ["Pears","apple","sweet"]
