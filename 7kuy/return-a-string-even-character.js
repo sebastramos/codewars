9db548ed09f9def421452d998a40f5eb4e22d3f3
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-12 21:13:49 +0200

    codewars challenge

--- 7kuy/return-a-string-even-character.js
+++ 7kuy/return-a-string-even-character.js
@@ -1,6 +1,7 @@
 // Write a function that returns a sequence (index begins with 1) of all the even characters from a string. 
 // If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
 
+// Solution 1
 function evenChars(string) {
 	if(string.length < 2 || string.length > 100) {
   	return "invalid string"
@@ -14,6 +15,10 @@ function evenChars(string) {
     return odds
   }
 }
+
+// Solution 2
+const evenChars = string =>
+  string.length < 2 || string.length > 100 ? `invalid string` : [...string].filter((_, idx) => idx % 2)
 
 
 console.log(evenChars("abcdefghijklm")) 
