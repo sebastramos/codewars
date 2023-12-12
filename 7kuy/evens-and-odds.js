61272013998b083f7ed54c922986c5e1c7cfa16f
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-12 20:58:36 +0200

    codewars challenge

--- 7kuy/evens-and-odds.js
+++ 7kuy/evens-and-odds.js
@@ -5,9 +5,14 @@
 // Numbers will be positive. 
 // The hexadecimal string should be lowercased.
 
+// Solution 1
 function evensAndOdds(num){
 	return num % 2 === 0 ? num.toString(2) : num.toString(16)
 }
+
+//Solution 2
+const evensAndOdds = n => n.toString(n%2 ? 16 : 2)
+
 
 console.log(evensAndOdds(2))
 //'10'
