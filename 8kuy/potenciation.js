a2e2c68e0ddb4dcd3e4d782c48ce7b52ec5f430c
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-16 20:11:51 +0200

    codewars challenge

---
+++ 8kuy/potenciation.js
@@ -0,0 +1,20 @@
+// The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+function power(x,y) {
+	return x ** y
+}
+
+console.log(power(1,701270))
+// 1
+console.log(power(2,2))
+// 4
+console.log(power(3,2))
+// 9
+console.log(power(-1,40))
+// 1
