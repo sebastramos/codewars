ee19d209926822bb4115e78b4c2c3862c5708f2a
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-19 17:20:02 +0200

    codewars challenge

--- 7kuy/bingo-or-not.js
+++ 7kuy/bingo-or-not.js
@@ -6,11 +6,12 @@
 // They do not need to be in the right order in the input array. Otherwise you will lose.
 // Your outputs should be "WIN" or "LOSE" respectively.
 
+// Solution 1
 function bingo(a) {
-	let word = 'BINGO'.split('')
+  let word = 'BINGO'.split('')
   let verify = ''
   
-	a = a.map(e => String.fromCharCode(e + 96).toUpperCase())
+  a = a.map(e => String.fromCharCode(e + 96).toUpperCase())
   let filtered = a.filter((e,i) => a.indexOf(e) === i)
   
   for(let i = 0; i < filtered.length; i++) {
@@ -20,6 +21,11 @@ function bingo(a) {
   }
   
   return word.length === verify.length ? 'WIN' : 'LOSE'
+}
+
+// Solution 2
+function bingo(a) {
+  return [2,9,14,7,15].every(e => a.includes(e)) ? "WIN" : "LOSE"
 }
 
 console.log(bingo([1,2,3,4,5,6,7,8,9,10]))
