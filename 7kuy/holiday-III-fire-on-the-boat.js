48641e2eeb9ff6087b3ec8131e8f7e57cca0cff9
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-21 14:17:51 +0200

    codewars challenge

--- 7kuy/holiday-III-fire-on-the-boat.js
+++ 7kuy/holiday-III-fire-on-the-boat.js
@@ -6,13 +6,19 @@
 // If any of these items are "Fire" you must spring into action.
 // Change any instance of "Fire" into "~~". Then return the string.
 
+// Solution 1
 function fireFight(s){
-    return s.split(' ').map(e => e === 'Fire' ? '~~' : e).join(' ')
-  }
+  return s.split(' ').map(e => e === 'Fire' ? '~~' : e).join(' ')
+}
+
+// Solution 2
+function fireFight(s){
+	return s.replaceAll('Fire', '~~')
+}
   
-  console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"))
-  // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
-  console.log(fireFight("Mast Deck Engine Water Fire"))
-  // "Mast Deck Engine Water ~~"
-  console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"))
-  // "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
+console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"))
+// "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
+console.log(fireFight("Mast Deck Engine Water Fire"))
+// "Mast Deck Engine Water ~~"
+console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"))
+// "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
