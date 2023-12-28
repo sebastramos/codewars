ec3c50e9bb914ad0e061ac3e09e8de92501d9bb8
Sebastian Ramos <sebramos.dev@gmail.com>
2023-12-28 21:07:36 +0200

    codewars challenge

--- 7kuy/seven-ate-9.js
+++ 7kuy/seven-ate-9.js
@@ -1,5 +1,6 @@
 // Write a function that removes every lone 9 that is inbetween 7s.
 
+// Solution 1
 function sevenAte9(str) {
 	let result = ''
   for(let i = 0; i < str.length; i++) { 	
@@ -14,6 +15,18 @@ function sevenAte9(str) {
     }
   }
   return result
+}
+
+// Solution 2
+function sevenAte9(str){
+  while (true) {
+    if(str.search('797') != -1) {
+      str = str.replace('797', '77')
+    } else {
+      break
+    }
+  }
+  return str
 }
 
 console.log(sevenAte9("79712312"))
