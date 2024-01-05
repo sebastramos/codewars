fcc191da442ddeda6b14bba559f51fef36a848eb
Sebastian Ramos <sebramos.dev@gmail.com>
2024-01-06 00:53:45 +0200

    codewars challenge

---
+++ 7kuy/find-sum-of-top-left-to-bottom-right-diagonals.js
@@ -0,0 +1,27 @@
+// Given a square matrix (i.e. an array of subarrays),
+// find the sum of values from the first value of the first array,
+// the second value of the second array,
+// the third value of the third array, and so on...
+
+function diagonalSum(matrix){
+	let sum = 0
+	for(let i = 0; i < matrix.length; i++) {
+  	sum += matrix[i][i]
+  }
+  return sum
+}
+
+console.log(diagonalSum([[12]]))
+// 12
+console.log(diagonalSum([[1, 2], 
+						 [3, 4]]))
+// 5
+console.log(diagonalSum([[1, 2, 3], 
+						 [4, 5, 6], 
+                         [7, 8, 9]]))
+// 15
+console.log(diagonalSum([[1, 2, 3, 4], 
+						 [5, 6, 7, 8], 
+                         [9, 10, 11, 12], 
+                         [13, 14, 15, 16]]))
+// 34
