fad40fdb566dc9acc569d0a25d76f06f5be87faa
Sebastian Ramos <sebramos.dev@gmail.com>
2024-01-11 18:27:09 +0200

    codewars challenge

--- 7kuy/double-every-other.js
+++ 7kuy/double-every-other.js
@@ -21,7 +21,11 @@ function doubleEveryOther(a) {
 	return a
 }
 
+// Solution 3
+const doubleEveryOther = (a) => a.map((c,i) => i % 2 == 0 ? c : 2 * c)
+
 console.log(doubleEveryOther([1,2,3,4]))
 // [1,4,3,8]
 
 console.log(doubleEveryOther([-10,7,-10,2,-7,-7,-7,6,-11,0]))
+// [-10, 14, -10, 4, -7, -14, -7, 12, -11, 0]
