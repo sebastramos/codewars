eff59ccc46603c02ea1ef8e241d14c56ef571d6a
Sebastian Ramos <sebramos.dev@gmail.com>
2024-01-13 22:53:47 +0200

    codewars challenge

--- 6kuy/lottery-ticket.js
+++ 6kuy/lottery-ticket.js
@@ -10,29 +10,35 @@
 // If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
 
 // All inputs will be in the correct format. Strings on tickets are not always the same length.
-
+ 
+// Solution 1
 function bingo(ticket, win){
 	
-    let letters = ticket.map(e => e[0].split(''))
-    
-    let miniWins = 0
-    for(let i = 0; i < letters.length; i++) {
-          for(let j = 0; j < letters[i].length; j++) {
-          if(letters[i][j].charCodeAt() === ticket[i][1]) {
-            miniWins++
-          break
-        }
+  let letters = ticket.map(e => e[0].split(''))
+  
+  let miniWins = 0
+  for(let i = 0; i < letters.length; i++) {
+		for(let j = 0; j < letters[i].length; j++) {
+    	if(letters[i][j].charCodeAt() === ticket[i][1]) {
+      	miniWins++
+        break
       }
     }
-      return miniWins >= win ? 'Winner!' : 'Loser!'
   }
-  
-  
-  console.log(bingo([['QDAFQN' ,75], ['NBSIP' ,80], ['RINOO' ,67], ['EKSL' ,81], ['DZI' ,66], ['DAM' ,84]], 1))
-  // 'Winner!'
-  console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2))
-  // 'Loser!'
-  console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1))
-  // 'Winner!'
-  console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3))
-  // 'Loser!'
+	return miniWins >= win ? 'Winner!' : 'Loser!'
+}
+
+// Solution 2
+function bingo(ticket, win){
+  ticket = ticket.filter(a => a[0].split('').some(b => b.charCodeAt(0) === a[1])).length 
+  return ticket >= win ? 'Winner!' : "Loser!"
+}
+
+console.log(bingo([['QDAFQN' ,75], ['NBSIP' ,80], ['RINOO' ,67], ['EKSL' ,81], ['DZI' ,66], ['DAM' ,84]], 1))
+// 'Winner!'
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2))
+// 'Loser!'
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1))
+// 'Winner!'
+console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3))
+// 'Loser!'
