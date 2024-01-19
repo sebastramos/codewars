ccf5b42570388945a67ea076730144c927240b7c
Sebastian Ramos <sebramos.dev@gmail.com>
2024-01-19 18:01:06 +0200

    codewars challenge

---
+++ 7kuy/true-for-just-one.js
@@ -0,0 +1,24 @@
+// Create a function called one that accepts two params:
+
+// a sequence
+// a function
+
+// and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+
+
+function one(arr, fun) {
+	let truthiness = []
+	for(let i = 0; i < arr.length; i++) {
+  	if(fun(arr[i])) {
+    	truthiness.push(true)
+    }
+  }
+	return truthiness.length === 1
+}
+
+console.log(one([1,2,3,4,5], function(item){ return item<2}))
+// true
+console.log(one([1,2,3,4,5], function(item){ return item%2 }))
+// false
+console.log(one([1,2,3,4,5], function(item){ return item>5 }))
+// false
