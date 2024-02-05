8026bb9421c1aca554808337dbc5a1f48f994d0f
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-05 14:19:10 +0200

    codewars challenge

--- 8kuy/correct-mistakes-character-recognition-software.js
+++ 8kuy/correct-mistakes-character-recognition-software.js
@@ -11,6 +11,7 @@
 // I is misinterpreted as 1
 // The test cases contain numbers only by mistake.
 
+// Solution 1
 function correct(string) {
 	let str = string.split('')
 
@@ -26,5 +27,30 @@ for(let i = 0; i < str.length; i++) {
   return str.join('')
 }
 
-console.log(correct('L1ND0N'))
-// 'LINDON'
+// Solution 2
+function correct(string) {
+	return string.split('')
+  						 .map(function (c) {
+               		if(c === '0') {
+                		return 'O'
+                	} else if(c === '1') {
+                 		return 'I' 	
+                  } else if(c === '5') {
+                  	return 'S'
+                  } else {
+                  	return c
+                  }
+               }).join('')
+}
+
+
+console.log(correct("L0ND0N"))
+// "LONDON"
+console.log(correct("DUBL1N"))
+// "DUBLIN"
+console.log(correct("51NGAP0RE"))
+// "SINGAPORE"
+console.log(correct("BUDAPE5T"))
+// "BUDAPEST"
+console.log(correct("PAR15"))
+// "PARIS"
