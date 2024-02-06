fb774205f47e46196be6ed488b2d0a33d4853d4e
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-06 18:45:14 +0200

    codewars challenge

--- 8kuy/no-zeros-for-heros.js
+++ 8kuy/no-zeros-for-heros.js
@@ -4,8 +4,19 @@
 
 // Get rid of them. Only the ending ones.
 
+// Zero alone is fine, don't worry about it.
+// Poor guy anyway
+
+// Solution 1
 function noBoringZeros(n) {
   return n % 10 || n === 0 ? n : noBoringZeros(n / 10);
+}
+
+// Solution 2
+function noBoringZeros(n) {
+  let reverseNum = +(String(Math.abs(n)).split('').reverse().join('')) 
+  let result = +(String(reverseNum).split('').reverse().join(''))
+	return n < 0 ? -1 * result : result
 }
 
 console.log(noBoringZeros(1450))
@@ -16,5 +27,5 @@ console.log(noBoringZeros(1050))
 // 105
 console.log(noBoringZeros(-1050))
 //-105
-// Zero alone is fine, don't worry about it.
-// Poor guy anyway
+console.log(noBoringZeros(0))
+// 0
