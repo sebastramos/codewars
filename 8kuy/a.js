b450250cda33ac595c07ebeba08f30dfc6892e97
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-07 08:43:14 +0200

    codewars challenge

---
+++ 8kuy/a.js
@@ -0,0 +1,18 @@
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+// Solution 1
+const a = '0'
+
+// Solution 2
+const a = []
+
+console.log(a == false)
+// true
+console.log(!a == false)
+// true
+console.log(a == !a)
+// true
