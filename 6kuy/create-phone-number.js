45df8b074390326a4c544c85607d51c97bd40b92
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-20 16:19:20 +0200

    codewars challenge

---
+++ 6kuy/create-phone-number.js
@@ -0,0 +1,21 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+function createPhoneNumber(numbers){
+  const npa = numbers.slice(0, 3).join('')
+  const coc = numbers.slice(3, 6).join('')
+  const cn = numbers.slice(6).join('')
+ 
+  return `(${npa}) ${coc}-${cn}`
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+// "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+// "(111) 111-1111"
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+// "(123) 456-7890"
