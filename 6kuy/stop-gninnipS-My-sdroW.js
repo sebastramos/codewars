5b3af66f9eee8fc6ea5b96c5d86bf3e879cd2865
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-20 16:54:00 +0200

    codewars challenge

---
+++ 6kuy/stop-gninnipS-My-sdroW.js
@@ -0,0 +1,33 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata).
+// Strings passed in will consist of only letters and spaces.
+// Spaces will be included only when more than one word is present.
+
+function spinWords(string) {
+	const str = string.split(' ')
+  let bigWord = str.filter(e => e.length >= 5)
+  let result = []
+  
+  for(let i = 0; i < str.length; i++) {
+  	if(!bigWord.includes(str[i])) {
+    	result.push(str[i])
+    } else {
+    	result.push(str[i].split('').reverse().join(''))
+    }
+  }
+  return result.join(' ')
+}
+
+console.log(spinWords("Welcome"))
+// "emocleW"
+console.log(spinWords("Hey fellow warriors"))
+// "Hey wollef sroirraw"
+console.log(spinWords("This is a test"))
+// "This is a test"
+console.log(spinWords("This is another test"))
+// "This is rehtona test"
+console.log(spinWords("You are almost to the last test"))
+// "You are tsomla to the last test"
+console.log(spinWords("Just kidding there is still one more"))
+// "Just gniddik ereht is llits one more")
+console.log(spinWords("Seriously this is the last one"))
+// "ylsuoireS this is the last one"
