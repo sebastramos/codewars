7f8c983956431d6b296ab0659eedff661cbee9c5
Sebastian Ramos <sebramos.dev@gmail.com>
2024-02-21 17:50:13 +0200

    codewars challenge

---
+++ 7kuy/words-to-sentence.js
@@ -0,0 +1,8 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+function wordsToSentence(words) {
+	return words.join(' ')
+}
+
+console.log(wordsToSentence(["hello", "world"]))
+// "hello world"
